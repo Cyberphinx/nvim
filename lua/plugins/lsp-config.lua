3d5de36d750b23a93676d3262d75c2b780236f9b
cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-03-10 22:42:32 +0000

    :heavy-plus-sign: none-ls linting and formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,35 @@
 return {
     {
-        -- Mason installs plugins for language servers
+        -- Mason-installs plugins for language servers
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
-        -- Mason lspconfig provides ensure installed functionality
+        -- Mason-lspconfig bridges the gap between mason and nvim and provides ensure installed functionality
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
+                ensure_installed = { "lua_ls", "tsserver" },
             })
-        end
+        end,
     },
     {
-        -- nvim lspconfig hooks up nvim with language servers to establish communication
+        -- Nvim-lspconfig hooks up nvim with language servers to establish communication
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
 
-            vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, {})
-        end
-    }
+            vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, {})
+            vim.keymap.set({ "n", "v" }, "<space>a", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
