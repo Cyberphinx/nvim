a12b0fcafafa83d4ed183ce0df8dfb7bcbb7023b
cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-03-10 18:14:21 +0000

    :sparkles: telescope treesitter

--- init.lua
+++ init.lua
@@ -2,7 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- LEADER
+vim.g.mapleader = " " -- works across all nvim files
 
+-- Lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -16,12 +19,36 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Lazy,nvim plugin manager's plugins installation
 local plugins = {
-  { "rose-pine/neovim", name = "rose-pine", priority = 1000 }
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
+-- Lazy.nvim's setup function
 require("lazy").setup(plugins, opts)
 
+-- Telescope fuzzy finder and live grep
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+
+-- Treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+-- Themes: Rose Pine
 require("rose-pine").setup()
 vim.cmd.colorscheme "rose-pine"
+
